5765ad629b43b80ae5a5935dfab6645654399e06
Zahid Afsar <zahidtempz@icloud.com>
2024-02-06 09:54:53 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+//State Variable Function   Hook(useState)
+//This state Variable is a int because its initial value is 0 an empty string
+
+  const [count, setCount] = useState(0);
+
+//This state Variable is a string because its initial value is '' an empty string
+  const [inputField, setInputField] = useState('');
+
+//what ever is in useState () <-- will be the initial value of the state value
+
+  const clickFunction = () => {
+    //inside of setCount we are updating count / altering the state
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button variant="primary" onClick={() => clickFunction()} >Iterate</Button>{' '}
+      <p>{count}</p>
+
+      <input onChange={(e) => setInputField(e.target.value)} ></input>
+      <p>{inputField}</p>
+    </>
   );
 }
 
